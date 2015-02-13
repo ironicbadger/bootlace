3acbe6f73f808fed9cbf110579116b44aca7be65
Alex Kretzschmar <alex.kretzschmar@proxama.com>
2015-02-13 14:00:26 +0000

    refactored to be more generic for other users than myself

--- bootlace.py
+++ bootlace.py
@@ -1,15 +1,62 @@
+# Bootlace - A simple Python app to send notifications via Pushover
+#
+# Author - Alex Kretzschmar
+
+from optparse import OptionParser
+import socket
 import requests
- 
-url = "https://api.pushover.net/1/messages.json"
- 
-data = {
-        'token':'',
-        'user':'',
-        'device':'epsilon',
-        'title':'Bootlace',
-        'message':'Epsilon has booted!'
+import sys
+
+url = 'https://api.pushover.net/1/messages.json'
+token = None
+user = None
+device = socket.gethostname()
+message = None
+title = 'Bootlace'
+
+parser = OptionParser()
+parser.add_option("-m", "--message", dest="message",
+                help="The message to be conveyed")
+parser.add_option("-d", "--device", dest="device",
+                  help="(Optional) Override device name")
+parser.add_option("-T", "--title", dest="title",
+                  help="(Optional) Override message title")
+parser.add_option("-t", "--token", dest="token",
+                  help="Pushover application token")
+parser.add_option("-u", "--user", dest="user",
+                  help="Pushover user token")
+
+option, content = parser.parse_args()
+
+if option.message:
+    message = option.message
+if option.device:
+    device = option.device
+if option.title:
+    title = option.title
+if option.token:
+    token = option.token
+if option.user:
+    user = option.user
+
+fields = (token, user, device, message, title)
+
+for f in fields:
+    if not f:
+        print('Required field not supplied.')
+        sys.exit(2)
+
+# Construct message using specified data
+
+constructed_message = {
+        'token':token,
+        'user':user,
+        'device':device,
+        'title':title,
+        'message':message
 }
- 
-r = requests.post(url, data)
- 
-print r
+
+# Send message using constructed message
+print constructed_message
+result = requests.post(url, constructed_message)
+print result
