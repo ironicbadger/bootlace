03d71d212bdfe8423411faf4e1954651892bc86a
Alex Kretzschmar <alex.kretzschmar@proxama.com>
2015-02-13 14:39:38 +0000

    Refactor using docopt

--- bootlace.py
+++ bootlace.py
@@ -1,62 +1,31 @@
-# Bootlace - A simple Python app to send notifications via Pushover
-#
-# Author - Alex Kretzschmar
+"""Bootlace - A simple Python app to send notifications via Pushover
 
-from optparse import OptionParser
-import socket
+Usage:
+  bootlace -m MESSAGE -t TOKEN -u USER [options]
+  bootlace --help
+
+Options:
+  -h, --help                       Show this help message and exit
+  -m MESSAGE, --message=MESSAGE    The message to be conveyed
+  -d DEVICE, --device=DEVICE       Override device name
+  -T TITLE, --title=TITLE          Override message title [default: Bootlace]
+  -t TOKEN, --token=TOKEN          Pushover application token
+  -u USER, --user=USER             Pushover user token
+"""
+
+import docopt
 import requests
-import sys
+import socket
 
 url = 'https://api.pushover.net/1/messages.json'
-token = None
-user = None
-device = socket.gethostname()
-message = None
-title = 'Bootlace'
+keys = ['token', 'user', 'device', 'message', 'title']
 
-parser = OptionParser()
-parser.add_option("-m", "--message", dest="message",
-                help="The message to be conveyed")
-parser.add_option("-d", "--device", dest="device",
-                  help="(Optional) Override device name")
-parser.add_option("-T", "--title", dest="title",
-                  help="(Optional) Override message title")
-parser.add_option("-t", "--token", dest="token",
-                  help="Pushover application token")
-parser.add_option("-u", "--user", dest="user",
-                  help="Pushover user token")
+args = docopt.docopt(__doc__)
 
-option, content = parser.parse_args()
+if not args['--device']:
+    args['--device'] = socket.gethostname()
 
-if option.message:
-    message = option.message
-if option.device:
-    device = option.device
-if option.title:
-    title = option.title
-if option.token:
-    token = option.token
-if option.user:
-    user = option.user
+constructed_message = {key: args['--'+key] for key in keys}
 
-fields = (token, user, device, message, title)
-
-for f in fields:
-    if not f:
-        print('Required field not supplied.')
-        sys.exit(2)
-
-# Construct message using specified data
-
-constructed_message = {
-        'token':token,
-        'user':user,
-        'device':device,
-        'title':title,
-        'message':message
-}
-
-# Send message using constructed message
-print constructed_message
 result = requests.post(url, constructed_message)
-print result
+print(result)
