431974a6b6a305e8fe68cdaa82a100373910657d
MichelfrancisBustillos <michelfrancisb@gmail.com>
2023-01-31 19:00:31 -0600

    Implement Timestamp Flag

--- bootlace.py
+++ bootlace.py
@@ -13,19 +13,24 @@ Options:
   -u USER, --user=USER              Pushover user token
   -p PRIORITY, --priority=PRIORITY  A value of -2, -1, 0 (default), 1, or 2. Only priority 1 or less is currently supported.
   --html                            Set to '1' to enable HTML prasing of message
+  --timestamp                       Replace timestamp of message receipt with time message was sent
 """
 
 import docopt
 import requests
 import socket
+import time
 
 url = 'https://api.pushover.net/1/messages.json'
-keys = ['token', 'user', 'device', 'message', 'title', 'html', 'priority']
+keys = ['token', 'user', 'device', 'message', 'title', 'html', 'priority', 'timestamp']
 
 args = docopt.docopt(__doc__)
 
 if args['--html']:  #If HTML flag set, set value to '1' to enable HTML parsing of MESSAGE
     args['--html'] = 1
+
+if args['--timestamp']: #If TIMESTAMP flag set, set value to current Unix time of host
+    args['--timestamp'] = int(time.time())
 
 if not args['--device']:  #If no device name provided, use Hostname
     args['--device'] = socket.gethostname()
